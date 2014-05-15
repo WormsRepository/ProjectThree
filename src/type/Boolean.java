68ac19b9adbe33fbac21d8919f700d3e03c6dd98
Pieter <Pieter@10.92.91.59>
2014-05-15 14:45:57 +0200

    added type boolean

---
+++ src/type/Boolean.java
@@ -0,0 +1,14 @@
+package type;
+
+public class Boolean extends T {
+	
+	public boolean getX(){
+		return this.x;
+	}
+	
+	public void setX(boolean x){
+		this.x = x;
+	}
+	
+	private boolean x = false;
+}
